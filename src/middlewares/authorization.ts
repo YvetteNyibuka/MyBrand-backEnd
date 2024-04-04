9f9de6361bf4dbd62bd6e2d93ab7a068bc8f9d31
YvetteNyibuka <izanybukayvette@gmail.com>
2024-04-04 14:35:03 +0200

    status updated

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -17,25 +17,25 @@ export const isAdmin = async (req: Request, res: Response, next: NextFunction) =
     const token:any = req.headers.authorization?.split(" ")[1]; 
     try{
         const decodedToken: any = jwt.verify(token,   process.env.MY_SECRET_KEY || "FYSHAFRW" ) as jwtPayload;
-        if(decodedToken.role !== 'admin' || !decodedToken)return res.status(403).json({message:"access denied"});
+        if(decodedToken.role !== 'admin' || !decodedToken)return res.status(403).json({message:"You have to login first"});
         next()
     } catch(err){
-        res.status(400).json({status: 'fail' , message: "Invalid token"})
+        res.status(500).json({status: 'fail' , message: "Internal Server Error"});
     }
 }
 export const isUser = async (req: Request, res: Response, next: NextFunction) => {
     const headerValues = req.headers.authorization;
     if(!headerValues){
-        return res.status(403).json({message: "access denied"});
+        return res.status(403).json({message: "You have to login first"});
     }
 
     const token:any = req.headers.authorization?.split(" ")[1]; 
     try{
         const decodedToken: any = jwt.verify(token,   process.env.MY_SECRET_KEY || "FYSHAFRW" ) as jwtPayload; 
-        if(!decodedToken.role)return res.status(403).json({message:"you are not logged in"});
+        if(!decodedToken.role)return res.status(403).json({message:"You have to login first"});
         next()
     } catch(err){
-        res.status(400).json({status: 'fail' , message: "Invalid token"})
+        res.status(500).json({status: 'fail' , message: "Internal Server Error"})
     }
 }
 
